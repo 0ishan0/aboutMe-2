72cc7025174e708b302554f2a0e1adc087e159e5
Ishan Bagchi <ishanbagchi@gmail.com>
2019-12-19 16:04:10 +0530

    Add smooth scroll effect in the website

--- main.js
+++ main.js
@@ -9,21 +9,47 @@ $(document).ready(function() {
     $('.top-nav').removeClass('open');
   });
 
-AOS.init({
-    easing: 'ease',
-    duration: 1800,
-    once: true
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1600, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  // Add smooth scrolling to #up
+  $('#up').on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: 0
+      }, 1600, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
 });
 
-// $('nav a[href*="#"]').on('click', function(){
-//   $('html, body').animate( keyframes: {
-//     scrollTop: $($(this).attr('href')).offset().top - 100
-//   }, options:2000);
-// });
-//
-// $('#up').on('click', function(){
-//   $('html, body').animate( keyframes: {
-//     scrollTop: 0
-//   }, options:2000);
-// });
+AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: false
+  });
